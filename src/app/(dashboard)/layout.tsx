521893b83885db8f2e8c8f1927ff6e9ffef3b797
Luis Dourado <lfaddourado@gmail.com>
2023-12-27 15:34:42 -0300

    feat: setup initial layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+import React, { type ReactNode } from "react";
+
+async function Layout({ children }: { children: ReactNode }) {
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <div className="flex max-h-screen min-h-screen min-w-full flex-col bg-background">
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
