cb28aed747d84133081dfc0f48700a57bbbaf588
Luis Dourado <lfaddourado@gmail.com>
2023-12-27 16:56:47 -0300

    feat: add form creation flow

---
+++ src/components/CreateFormBtn.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Loader2 } from "lucide-react";
+import { toast } from "./ui/use-toast";
+import { type FormSchema, formSchema } from "@/schemas/form";
+import { CreateForm } from "@/actions/form";
+
+function CreateFormBtn() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  });
+  async function onSubmit(value: FormSchema) {
+    try {
+      const formId = await CreateForm(value);
+      toast({
+        title: "Formulário criado",
+        description: "Seu formulário foi criado com sucesso.",
+      });
+      console.log(formId);
+    } catch (error) {
+      toast({
+        title: "Erro ao criar formulário",
+        description:
+          "Algo deu errado ao criar o formulário, tente novamente mais tarde.",
+        variant: "destructive",
+      });
+    }
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Criar novo formulário</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Criar Formulário</DialogTitle>
+          <DialogDescription>
+            Crie um novo formulário para começar a receber submissões.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome do Formulário</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Descrição do Formulário</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={form.formState.isSubmitting}
+            className="mt-4 w-full"
+          >
+            {!form.formState.isSubmitting && "Criar Formulário"}
+            {form.formState.isSubmitting && (
+              <Loader2 className="animate-spin" />
+            )}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateFormBtn;
