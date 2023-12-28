dff53bd639e073e28f63ccad0a2102247a67931e
Luis Dourado <lfaddourado@gmail.com>
2023-12-27 21:57:41 -0300

    feat: add DesignerContextProvider to layout component

--- src/app/(dashboard)/form-builder/[id]/layout.tsx
+++ src/app/(dashboard)/form-builder/[id]/layout.tsx
@@ -1,7 +1,12 @@
+import DesignerContextProvider from "@/components/context/DesignerContext";
 import { type ReactNode } from "react";
 
 function layout({ children }: { children: ReactNode }) {
-  return <div className="mx-auto flex w-full flex-grow">{children}</div>;
+  return (
+    <DesignerContextProvider>
+      <div className="mx-auto flex w-full flex-grow">{children}</div>;
+    </DesignerContextProvider>
+  );
 }
 
 export default layout;
