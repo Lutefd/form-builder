5c63064808190830c4cf91ea87d5c63c281d81bf
Luis Dourado <lfaddourado@gmail.com>
2023-12-28 02:23:10 -0300

    feat: Add new form elements and update sidebar

--- src/components/FormElements.tsx
+++ src/components/FormElements.tsx
@@ -1,8 +1,27 @@
 import { TextFieldFormElement } from "./fields/TextField";
 import { TitleFieldFormElement } from "./fields/TitleField";
 import { SubTitleFieldFormElement } from "./fields/SubtitleField";
+import { ParagprahFieldFormElement } from "./fields/ParagraphField";
+import { SeparatorFieldFormElement } from "./fields/SeparatorField";
+import { SpacerFieldFormElement } from "./fields/SpacerField";
+import { NumberFieldFormElement } from "./fields/NumberField";
+import { TextAreaFormElement } from "./fields/TextAreaField";
+import { DateFieldFormElement } from "./fields/DateField";
+import { SelectFieldFormElement } from "./fields/SelectField";
+import { CheckboxFieldFormElement } from "./fields/CheckboxField";
 
-export type ElementsType = "TextField" | "TitleField" | "SubTitleField";
+export type ElementsType =
+  | "TextField"
+  | "TitleField"
+  | "SubTitleField"
+  | "ParagraphField"
+  | "SeparatorField"
+  | "SpacerField"
+  | "NumberField"
+  | "TextAreaField"
+  | "DateField"
+  | "SelectField"
+  | "CheckboxField";
 export type SubmitFunction = (key: string, value: string) => void;
 
 export type FormElement = {
@@ -41,4 +60,12 @@ export const FormElements: FormElements = {
   TextField: TextFieldFormElement,
   TitleField: TitleFieldFormElement,
   SubTitleField: SubTitleFieldFormElement,
+  ParagraphField: ParagprahFieldFormElement,
+  SeparatorField: SeparatorFieldFormElement,
+  SpacerField: SpacerFieldFormElement,
+  NumberField: NumberFieldFormElement,
+  TextAreaField: TextAreaFormElement,
+  DateField: DateFieldFormElement,
+  SelectField: SelectFieldFormElement,
+  CheckboxField: CheckboxFieldFormElement,
 };
--- src/components/FormElementsSidebar.tsx
+++ src/components/FormElementsSidebar.tsx
@@ -5,7 +5,7 @@ import { Separator } from "./ui/separator";
 
 function FormElementsSidebar() {
   return (
-    <div>
+    <div className="bar-t">
       <p className="text-sm text-foreground/70">Elementos de Drag and Drop</p>
       <Separator className="my-2" />
       <div className="grid grid-cols-1 place-items-center gap-2 md:grid-cols-2">
@@ -14,11 +14,19 @@ function FormElementsSidebar() {
         </p>
         <SidebarBtnElement formElement={FormElements.TitleField} />
         <SidebarBtnElement formElement={FormElements.SubTitleField} />
+        <SidebarBtnElement formElement={FormElements.ParagraphField} />
+        <SidebarBtnElement formElement={FormElements.SeparatorField} />
+        <SidebarBtnElement formElement={FormElements.SpacerField} />
 
         <p className="col-span-1 my-2 place-self-start text-sm text-muted-foreground md:col-span-2">
           Elementos de Formulário
         </p>
         <SidebarBtnElement formElement={FormElements.TextField} />
+        <SidebarBtnElement formElement={FormElements.NumberField} />
+        <SidebarBtnElement formElement={FormElements.TextAreaField} />
+        <SidebarBtnElement formElement={FormElements.DateField} />
+        <SidebarBtnElement formElement={FormElements.SelectField} />
+        <SidebarBtnElement formElement={FormElements.CheckboxField} />
       </div>
     </div>
   );
