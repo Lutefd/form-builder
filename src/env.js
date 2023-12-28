b871c8b97622fc7c04b07afcf1a97a37cdc453ad
Luis Dourado <lfaddourado@gmail.com>
2023-12-28 00:39:40 -0300

    feat: Add NEXT_PUBLIC_WEBSITE_URL to client and server env

--- src/env.js
+++ src/env.js
@@ -38,7 +38,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_WEBSITE_URL: z.string(),
   },
 
   /**
@@ -52,6 +52,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
