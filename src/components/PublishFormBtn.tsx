9b813884466148b9c357422b61f11c74898602ef
Luis Dourado <lfaddourado@gmail.com>
2023-12-28 00:41:35 -0300

    feat: Add publish form button with confirmation dialog

--- src/components/PublishFormBtn.tsx
+++ src/components/PublishFormBtn.tsx
@@ -1,13 +1,83 @@
 import React from "react";
+import { BookOpenCheck, Loader2 } from "lucide-react";
+import { PublishForm } from "@/actions/form";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 import { Button } from "./ui/button";
-import { BookOpenCheck } from "lucide-react";
+import { toast } from "./ui/use-toast";
+import useDesigner from "./hooks/useDesigner";
 
-function PublishFormBtn() {
+function PublishFormBtn({ id }: { id: string }) {
+  const [loading, startTransition] = useTransition();
+  const { elements } = useDesigner();
+
+  const router = useRouter();
+
+  async function publishForm() {
+    try {
+      const JSONElements = JSON.stringify(elements);
+
+      await PublishForm(id, JSONElements);
+      toast({
+        title: "Successo",
+        description: "Seu formulário foi publicado com sucesso.",
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Erro",
+        description: "Algo deu errado ao publicar o formulário.",
+        variant: "destructive",
+      });
+    }
+  }
+
   return (
-    <Button variant="outline" className="gap-2">
-      <BookOpenCheck className="h-5 w-5" />
-      Publicar
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="gap-2">
+          <BookOpenCheck className="h-5 w-5" />
+          Publicar{" "}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta ação não pode ser desfeita. Após publicar o formulário, você
+            não poderá mais editá-lo.
+            <br />
+            <br />
+            <span className="font-medium">
+              Ao publicar o formulário, você o torna disponível para o público.
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e: React.MouseEvent) => {
+              e.preventDefault();
+              startTransition(publishForm);
+            }}
+          >
+            Publicar {loading && <Loader2 className="animate-spin" />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
