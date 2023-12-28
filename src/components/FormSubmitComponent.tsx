899e8c960bc9b61974c42e19a01cb75bec814fb4
Luis Dourado <lfaddourado@gmail.com>
2023-12-28 01:07:17 -0300

    feat: add FormSubmitComponent to handle form submission

---
+++ src/components/FormSubmitComponent.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import React, { useCallback, useRef, useState, useTransition } from "react";
+import { type FormElementInstance, FormElements } from "./FormElements";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import { SubmitForm } from "@/actions/form";
+import { Loader2, Send } from "lucide-react";
+
+function FormSubmitComponent({
+  formUrl,
+  content,
+}: {
+  content: FormElementInstance[];
+  formUrl: string;
+}) {
+  const formValues = useRef<Record<string, string>>({});
+  const formErrors = useRef<Record<string, boolean>>({});
+  const [renderKey, setRenderKey] = useState(new Date().getTime());
+
+  const [submitted, setSubmitted] = useState(false);
+  const [pending, startTransition] = useTransition();
+
+  const validateForm: () => boolean = useCallback(() => {
+    for (const field of content) {
+      const actualValue = formValues.current[field.id] ?? "";
+      const valid = FormElements[field.type].validate(field, actualValue);
+
+      if (!valid) {
+        formErrors.current[field.id] = true;
+      }
+    }
+
+    if (Object.keys(formErrors.current).length > 0) {
+      return false;
+    }
+
+    return true;
+  }, [content]);
+
+  const submitValue = useCallback((key: string, value: string) => {
+    formValues.current[key] = value;
+  }, []);
+
+  const submitForm = async () => {
+    formErrors.current = {};
+    const validForm = validateForm();
+    if (!validForm) {
+      setRenderKey(new Date().getTime());
+      toast({
+        title: "Erro",
+        description: "Seu formulário contém erros",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    try {
+      const jsonContent = JSON.stringify(formValues.current);
+      await SubmitForm(formUrl, jsonContent);
+      setSubmitted(true);
+    } catch (error) {
+      toast({
+        title: "Erro",
+        description: "Algo deu errado ao enviar o formulário",
+        variant: "destructive",
+      });
+    }
+  };
+
+  if (submitted) {
+    return (
+      <div className="flex h-full w-full items-center justify-center p-8">
+        <div className="flex w-full max-w-[620px] flex-grow flex-col gap-4 overflow-y-auto rounded border bg-background p-8 ">
+          <h1 className="text-2xl font-bold">Formulário Enviado</h1>
+          <p className="text-muted-foreground">
+            Obrigado por enviar o formulário! Você pode fechar esta página.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-full w-full items-center justify-center p-8">
+      <div
+        key={renderKey}
+        className="flex w-full max-w-[620px] flex-grow flex-col gap-4 overflow-y-auto rounded border bg-background p-8 "
+      >
+        {content.map((element) => {
+          const FormElement = FormElements[element.type].formComponent;
+          return (
+            <FormElement
+              key={element.id}
+              elementInstance={element}
+              submitValue={submitValue}
+              isInvalid={formErrors.current[element.id]}
+              defaultValue={formValues.current[element.id]}
+            />
+          );
+        })}
+        <Button
+          className="mt-8"
+          onClick={() => {
+            startTransition(submitForm);
+          }}
+          disabled={pending}
+        >
+          {!pending && (
+            <>
+              <Send className="mr-2" />
+              Enviar
+            </>
+          )}
+          {pending && <Loader2 className="animate-spin" />}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default FormSubmitComponent;
