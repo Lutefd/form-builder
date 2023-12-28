ed9b2e69fbc0de187ee5a62851108a6298136fad
Luis Dourado <lfaddourado@gmail.com>
2023-12-27 22:05:15 -0300

    feat: add FormElementsSidebar component

--- src/components/DesignerSidebar.tsx
+++ src/components/DesignerSidebar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { FormElements } from "./FormElements";
 import SidebarBtnElement from "./SidebarBtnElement";
+import useDesigner from "./hooks/useDesigner";
+import FormElementsSidebar from "./FormElementsSidebar";
 
 function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
   return (
     <aside className="flex h-full w-[400px] max-w-[400px] flex-col gap-2 overflow-y-auto border-l-2 border-muted bg-background p-4">
-      Elementos
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   );
 }
